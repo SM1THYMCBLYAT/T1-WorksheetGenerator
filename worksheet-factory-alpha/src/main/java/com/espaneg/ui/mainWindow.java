02f86dc37cc6f3e3faa119489b2f1d4b2f1e61ba
PaygeDenysschen <Paygedenysschen@gmail.com>
2025-11-10 18:13:21 +0200

    Header and logo added

---
+++ worksheet-factory-alpha/src/main/java/com/espaneg/ui/mainWindow.java
@@ -0,0 +1,55 @@
+package com.espaneg.ui;
+/*;
+used for user interface, such as Swing UI
+ */
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+
+public class mainWindow {
+    public static void main(String[] args) {
+
+        ImageIcon image= new ImageIcon("LOGO2.png");
+        Border headBorder1 = BorderFactory.createLineBorder(Color.WHITE,4);
+
+
+        JLabel label = new JLabel(); //First text label
+        label.setText("Educreate"); //Sets label text
+        label.setIcon(image);
+
+
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setVerticalAlignment(JLabel.CENTER); //label position in frame
+        label.setHorizontalAlignment(JLabel.CENTER); // label position in frame
+
+        label.setForeground(new Color(0xffffff)); //set font color
+        label.setFont(new Font("Inter",Font.PLAIN,96)); // set font
+        label.setOpaque(true);
+        label.setBorder(headBorder1);
+
+
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Educreate"); //text box name
+        frame.setSize(1920,880); //frame size
+        frame.setVisible(true); //visibility of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit button closes
+
+        ImageIcon logo = new ImageIcon("LOGO.png"); //textbox logo
+        frame.setIconImage(logo.getImage());
+        frame.getContentPane().setBackground(new Color(0xdaf0ff)); //back color
+
+        frame.add(label); //adds label to frame
+        frame.revalidate(); //refreshes adding text to already made frame
+
+
+
+
+
+
+
+    }
+}
